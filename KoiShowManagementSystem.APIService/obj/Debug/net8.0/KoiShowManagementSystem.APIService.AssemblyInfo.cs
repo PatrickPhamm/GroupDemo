629d77944219a5eb08b001056e397d4d779ec465
dothephi <phidtse161229@fpt.edu.vn>
2024-10-17 09:00:07 +0700

    Phi update

--- KoiShowManagementSystem.APIService/obj/Debug/net8.0/KoiShowManagementSystem.APIService.AssemblyInfo.cs
+++ KoiShowManagementSystem.APIService/obj/Debug/net8.0/KoiShowManagementSystem.APIService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("KoiShowManagementSystem.APIService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6346984c2c3a04bc5d0838a42484319ecba1d806")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece7a5a891024622ea52d9e9affa5b181b2818ae")]
 [assembly: System.Reflection.AssemblyProductAttribute("KoiShowManagementSystem.APIService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("KoiShowManagementSystem.APIService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
